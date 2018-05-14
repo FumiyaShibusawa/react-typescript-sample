938a8695a40753322456b4cf89e11745bed7091e
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-05-14 23:39:03 +0900

    update: reactのDOMたちを作成＆typescriptへの対応（途中）

---
+++ app/javascript/packs/components/Todo.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+interface TodoProps {
+  todo_name: string,
+  status: string
+}
+
+class Todo extends React.Component<TodoProps, {}> {
+  render(){
+    return (
+      <div>
+        <ul>
+          <li>
+            <div>{ this.props.status }</div>
+            { this.props.todo_name }
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default Todo
